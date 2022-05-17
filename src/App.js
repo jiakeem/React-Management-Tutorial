cbcfe239f96e7bd87689dde7832f0838f23f80eb
jiakeem <kyo38439@gmail.com>
2022-05-17 17:09:21 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./App.css";
 import Customer from "./components/Customers";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from "@mui/material";
+import { withStyles } from "@mui/styles";
+import { createTheme } from "@mui/system";
+
+const styles = () => ({
+  root: {
+    width: "100%",
+    marginTop: createTheme().spacing(3),
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -29,22 +50,36 @@ const customers = [
   },
 ];
 
-const App = () => {
+const App = ({ classes }) => {
   return (
-    <div>
-      {customers.map((c) => (
-        <Customer
-          key={c.id}
-          id={c.id}
-          image={c.image}
-          name={c.name}
-          birthday={c.birthday}
-          gender={c.gender}
-          job={c.job}
-        />
-      ))}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c) => (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 };
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { TableRow, TableCell } from "@mui/material";
 
 const Customer = ({ id, image, name, birthday, gender, job }) => {
   return (
-    <div>
-      <CustomerProfile id={id} image={image} name={name} />
-      <CustomerInfo birthday={birthday} gender={gender} job={job} />
-    </div>
+    // <div>
+    //   <CustomerProfile id={id} image={image} name={name} />
+    //   <CustomerInfo birthday={birthday} gender={gender} job={job} />
+    // </div>
+    <TableRow>
+      <TableCell>{id}</TableCell>
+      <TableCell>
+        <img src={image} alt="profile" />
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell>{birthday}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>{job}</TableCell>
+    </TableRow>
   );
 };
 
-const CustomerProfile = ({ id, image, name }) => {
+/* const CustomerProfile = ({ id, image, name }) => {
   return (
     <div>
       <img src={image} alt="profile" />
@@ -28,6 +39,6 @@ const CustomerInfo = ({ birthday, gender, job }) => {
       <p>{job}</p>
     </div>
   );
-};
+}; */
 
 export default Customer;
