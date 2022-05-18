b97e36200d2eaf5136c63d02c72a3519cacadf13
jiakeem <kyo38439@gmail.com>
2022-05-18 17:21:23 +0900

    Add Get Customers REST API modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Customer from "./components/Customers";
 import {
@@ -23,34 +23,20 @@ const styles = () => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "신혜림",
-    birthday: "961210",
-    gender: "남자",
-    job: "배달의민족",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "정가연",
-    birthday: "970531",
-    gender: "남자",
-    job: "넥센타이어",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "이현주",
-    birthday: "980218",
-    gender: "남자",
-    job: "더존",
-  },
-];
-
 const App = ({ classes }) => {
+  const [customers, setCustomers] = useState([]);
+
+  const callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    console.log(body);
+    return body;
+  };
+
+  useEffect(() => {
+    callApi().then((data) => setCustomers(data));
+  }, []);
+
   return (
     <Paper className={classes.root}>
       <Table className={classes.table}>
--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //REST API로 데이터 주고받기 위함
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "신혜림",
+      birthday: "961210",
+      gender: "남자",
+      job: "배달의민족",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "정가연",
+      birthday: "970531",
+      gender: "남자",
+      job: "넥센타이어",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "이현주",
+      birthday: "980218",
+      gender: "남자",
+      job: "더존",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
